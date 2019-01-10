84ef1ba340f1e785fd0d52c356529525f4811e02
Marek <mrkkr91@gmail.com>
2019-01-10 15:00:22 +0100

    Cleaning up a rails routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  resources :quotes
+  resources :quotes, :only => [:index, :new, :create] do
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "quotes#index"
 end
